a0765aa109cc730cc318badff91df18c096c9b43
mohan8991 <mohan.8991@gmail.com>
2016-07-28 04:02:23 -0500

    feat(module-api): UserInfo POJO

---
+++ server/movie-rest/src/main/java/io/egen/rest/entity/UserInfo.java
@@ -0,0 +1,76 @@
+package io.egen.rest.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table
+@NamedQueries({
+	@NamedQuery(name = "UserInfo.findAll", query = "SELECT e FROM UserInfo e ORDER BY e.lastName ASC"),
+	@NamedQuery(name = "UserInfo.findOne", query = "SELECT e FROM UserInfo e WHERE e.userName=:pUserName")
+})
+public class UserInfo {
+	
+	@Id
+	@GenericGenerator(name = "customUUID", strategy = "uuid2")
+	@GeneratedValue(generator = "customUUID")
+	private String id;
+	
+	private String firstName;
+	private String lastName;
+	
+	@Column(unique = true)
+	private String userName;
+	
+	private String password;
+	private String role;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	@Override
+	public String toString() {
+		return "UserInfo [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", userName=" + userName
+				+ ", password=" + password + ", role=" + role + "]";
+	}
+}
