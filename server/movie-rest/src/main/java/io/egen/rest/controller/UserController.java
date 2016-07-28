c535a2b467cdcb28c9976e44feae8fb8f86e189b
mohan8991 <mohan.8991@gmail.com>
2016-07-28 03:59:03 -0500

    feat(module-api): Added invalid user Exception

---
+++ server/movie-rest/src/main/java/io/egen/rest/controller/UserController.java
@@ -0,0 +1,44 @@
+package io.egen.rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.egen.rest.entity.UserInfo;
+import io.egen.rest.service.UserService;
+
+@RestController
+@RequestMapping(path = "UserInfo")
+public class UserController {
+
+	@Autowired
+	UserService service;
+	
+	@RequestMapping(method = RequestMethod.GET, path = "{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public UserInfo findOne(@PathVariable("id") String userId) {
+		return service.findOne(userId);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public UserInfo create(@RequestBody UserInfo uInfo) {
+		return service.create(uInfo);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, path = "{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public UserInfo update(@PathVariable("id") String userId, @RequestBody UserInfo uInfo) {
+		return service.update(userId, uInfo);
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, path = "{id}")
+	public void delete(@PathVariable("id") String userId) {
+		service.delete(userId);
+	}
+	
+	public String aReq(String UserName){
+		return service.aReq(UserName);
+	}
+}
